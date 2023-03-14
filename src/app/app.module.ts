49c6ce2fd47513fd9ff4dd7d1486c28974a9c593
Ian Jackson <iansjackson8@gmail.com>
2023-03-14 11:40:44 -0400

    add header and images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ImageCarouselComponent
   ],
   imports: [
     BrowserModule,
